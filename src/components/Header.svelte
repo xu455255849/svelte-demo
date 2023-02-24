b467f505fd533f24427b6bfa8f129ae46785baa3
xushaoping <xusp@yunxi.tv>
2023-02-24 14:50:22 +0800

    code update

---
+++ src/components/Header.svelte
@@ -0,0 +1,5 @@
+<p>the value is {value}</p>
+
+<script>
+	export let value;
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit { name }</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2>{@html html}</h2>
+
+<button on:click="{handleClick}"> count add</button>
+{count} double: {double}
+
+<p>Arr listen {numbers.join(' + ')} = {sum}</p>
+
+<button on:click="{addNumber}"> num add</button>
+
+
+<Header value={count} />
+
+
+
+<script>
+	import Header from '../components/Header.svelte'
+
+	const name =  'Xushaopign'
+	const html = 'this is <span>qweqe</span>'
+
+	let numbers = [1,2,3]
+	$: sum = numbers.reduce((prev, cur) => prev + cur, 0)
+
+	function addNumber() {
+		numbers.push(numbers.length + 1)
+		numbers = numbers;
+	}
+
+	let count = 0;
+	$: double = count * 2
+	$: {
+		console.log(111, `${count}`)
+	}
+	$: if (count > 10) {
+		alert(`count value is high ${count}`)
+		count = 0
+	}
+
+	function handleClick () {
+		count += 1
+	}
+
+</script>
+
+<style>
+	p {
+			color: aqua;
+			font-size: 24px;
+	}
+
+	h2 {
+			color: rebeccapurple;
+	}
+</style>
